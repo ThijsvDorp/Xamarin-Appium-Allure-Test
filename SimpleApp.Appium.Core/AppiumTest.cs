ad5fe70d1ca6a67716ad56fe39aed1127c7f2c94
Thijs <Thijsjuh97@hotmail.com>
2021-05-18 10:30:24 +0200

    T

---
+++ SimpleApp.Appium.Core/AppiumTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+
+namespace SimpleApp.Appium.Core
+{
+    [SetUpFixture]
+    public abstract class AppiumTest<T, W>
+        where T : AppiumDriver<W>
+        where W : IWebElement
+    {
+        protected AppiumTest(string testName)
+        {
+            //Where the host is located
+            driverUri = new Uri("http://localhost:4723/wd/hub");
+            appiumOptions.AddAdditionalCapability("testName", testName);
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.FullReset, "false");
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            InitAppiumOptions(appiumOptions);
+            appiumDriver = GetDriver();
+        }
+
+        [OneTimeTearDown()]
+        public void TearDown()
+        {
+            // Perform a driver quit so that the report is printed
+            appiumDriver.Quit();
+        }
+
+        protected abstract T GetDriver();
+        protected abstract void InitAppiumOptions(AppiumOptions appiumOptions);
+        protected T appiumDriver;
+        protected readonly AppiumOptions appiumOptions;
+        protected readonly Uri driverUri;
+
+        public string GetElementText(string elementId)
+        {
+            var element = appiumDriver.FindElementByAccessibilityId(elementId);
+            var attributName = IsAndroid ? "text" : "value";
+            return element.GetAttribute(attributName);
+        }
+
+        public bool IsAndroid => appiumDriver.Capabilities.GetCapability(MobileCapabilityType.PlatformName).Equals("Android");
+    }
+
+
+}
---
+++ SimpleApp.Appium.Core/TestMainPage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+
+namespace SimpleApp.Appium.Core
+{
+    [AllureNUnit]
+    public class TestMainPage<T, W>: AppiumTest<T, W>
+        where T : AppiumDriver<W>
+        where W : IWebElement
+    {
+        public TestMainPage(string testName): base(testName)
+        {
+        }
+
+        protected override T GetDriver()
+        {
+            // Implemented by platform specific class
+            throw new NotImplementedException();
+        }
+
+        protected override void InitAppiumOptions(AppiumOptions appiumOptions)
+        {
+            // Implemented by platform specific class
+            throw new NotImplementedException();
+        }
+
+        [SetUp()]
+        public void SetupTest()
+        {
+            //appiumDriver.CloseApp();
+            //appiumDriver.LaunchApp();
+        }
+
+        [Test()]
+
+        public void TestLogin()
+        {
+            appiumDriver.FindElementByAccessibilityId("Login").Click();
+            appiumDriver.FindElementByAccessibilityId("UserName").SendKeys("user@email.com");
+            appiumDriver.FindElementByAccessibilityId("Password").SendKeys("Password");
+
+            appiumDriver.FindElementByAccessibilityId("LoginButton").Click();
+            var text = GetElementText("StatusLabel"); // Android is "text"
+
+            Assert.IsNotNull(text);
+            Assert.IsTrue(text.StartsWith("Logging in", StringComparison.CurrentCulture));  
+        }
+
+        [Test()]
+        public void TestAddItem()
+        {
+            appiumDriver.FindElementByAccessibilityId("Browse").Click();
+            appiumDriver.FindElementByAccessibilityId("AddToolbarItem").Click();
+            var itemNameField = appiumDriver.FindElementByAccessibilityId("ItemNameEntry");
+            itemNameField.SendKeys("todo");
+
+            var itemDesriptionField = appiumDriver.FindElementByAccessibilityId("ItemDescriptionEntry");
+            itemDesriptionField.SendKeys("todo description");
+
+            appiumDriver.FindElementByAccessibilityId("SaveToolbarItem").Click();
+        }
+
+        [Test()]
+        public void TestAbout()
+        {
+            appiumDriver.FindElementByAccessibilityId("About").Click(); // works for iOS
+        }
+    }
+}
